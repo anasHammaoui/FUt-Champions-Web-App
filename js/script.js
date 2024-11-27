af41a37bfaef3acf390bc3f2a81998523764bacc
anashm <anashammaoui07@gmail.com>
2024-11-27 16:12:27 +0100

    FC-4 complete cards page

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ document.getElementById("add-btn").addEventListener("click",function (){
     document.querySelector(".form-adding").classList.remove("hide");
 console.log(document.querySelector(".form-adding"))
 })
-document.querySelector(".fa-xmark").addEventListener("click",()=>{
+document.querySelector(".add-p").addEventListener("click",()=>{
     document.querySelector(".form-adding").classList.add("hide");
+})
+// show cards
+document.getElementById("show-btn").addEventListener("click",function (){
+    document.querySelector(".view-cards").classList.remove("hide");
+console.log(document.querySelector(".form-adding"))
+})
+document.querySelector(".out-card").addEventListener("click",()=>{
+    document.querySelector(".view-cards").classList.add("hide");
 })
